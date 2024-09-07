5c112220f69f340e8496566178634be0bd791b39
Seya <31957516+sglkc@users.noreply.github.com>
2024-09-07 20:15:16 +0700

    feat(routes): add error, layout, & index page
    refactor(routes): move original index page to read
    style(uno): add styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,20 @@
 <script lang="ts">
 import '@unocss/reset/tailwind.css'
 import 'virtual:uno.css'
+import '@fontsource-variable/sora'
+import '@fontsource-variable/bricolage-grotesque'
 </script>
 
-<slot></slot>
+<div class="bg-base text-base font-body min-h-100svh flex flex-col justify-between">
+  <header class="px-8 py-4 b-base b-b-4 flex justify-between items-center font-heading fw-bold">
+    <h1 class="text-8">YOMIKATA</h1>
+  </header>
+  <main class="p-8">
+    <slot />
+  </main>
+  <footer class="p-8">
+    <p>
+      Yomikata | Made by <a href="https://github.com/sglkc">sglkc</a>
+    </p>
+  </footer>
+</div>
--- uno.config.ts
+++ uno.config.ts
@@ -1,4 +1,38 @@
 import { defineConfig } from 'unocss'
 
 export default defineConfig({
+  shortcuts: {
+    'bg-base': 'bg-white dark:bg-black',
+    'text-base': 'text-gray-900 dark:text-gray-300',
+    'b-base': 'b-black dark:border-white',
+    'colors-primary': 'bg-black text-white dark:bg-white dark:text-black',
+    'colors-secondary': 'bg-white text-black dark:bg-black dark:text-white',
+  },
+  theme: {
+    fontFamily: {
+      heading: '"Bricolage Grotesque Variable", sans-serif',
+      body: '"Sora Variable", sans-serif',
+    },
+    backgroundColor: {
+      primary: '',
+      secondary: '',
+    },
+    borderColor: {
+      primary: '',
+      secondary: '',
+    },
+    textColor: {
+      primary: '',
+      secondary: '',
+    },
+  },
+  variants: [
+    (matcher) => {
+      if (!matcher.startsWith('on:')) return matcher
+      return {
+        matcher: matcher.slice(3),
+        selector: s => `${s}:hover, ${s}:focus`,
+      }
+    },
+  ],
 })
