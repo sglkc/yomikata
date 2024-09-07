d83256723786afe13748a451ec15ae9749b685a8
Seya <31957516+sglkc@users.noreply.github.com>
2024-09-07 21:04:46 +0700

    feat(components): add Link

---
+++ src/lib/components/Link.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+import type { HTMLAnchorAttributes } from 'svelte/elements'
+import clsx from 'clsx'
+
+interface $$Props extends HTMLAnchorAttributes {
+  href: string
+}
+
+let { class: className, href, ...rest } = $$props
+</script>
+
+<a
+  class={clsx(
+    'b-base b-b-2 transition-[border] on:b-b-4',
+    className
+  )}
+  href={href}
+  {...rest}
+>
+  <slot />
+</a>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@ import '@unocss/reset/tailwind.css'
 import 'virtual:uno.css'
 import '@fontsource-variable/sora'
 import '@fontsource-variable/bricolage-grotesque'
+import Link from '$lib/components/Link.svelte'
 </script>
 
 <div class="bg-base text-base font-body min-h-100svh flex flex-col justify-between">
@@ -12,9 +13,12 @@ import '@fontsource-variable/bricolage-grotesque'
   <main class="p-8">
     <slot />
   </main>
-  <footer class="p-8">
+  <footer class="p-8 b-base b-t-4 flex justify-between">
     <p>
-      Yomikata | Made by <a href="https://github.com/sglkc">sglkc</a>
+      Made with love by <Link href="https://github.com/sglkc">sglkc</Link>.
+    </p>
+    <p>
+      MIT License &copy; 2024
     </p>
   </footer>
 </div>
